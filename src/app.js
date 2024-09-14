f2e5c56acb8ff4ac4c5b4e7f3a459b232a6ca5f4
zlinzz <z332119551@gmail.com>
2024-09-14 01:50:53 -0400

    Move health check to src/routes/index.js, add initial fragments API setup

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const compression = require('compression');
 
 // author and version from our package.json file
 // TODO: make sure you have updated your name in the `author` section
-const { author, version } = require('../package.json');
+// const { author, version } = require('../package.json');
 
 const logger = require('./logger');
 const pino = require('pino-http')({
@@ -30,22 +30,8 @@ app.use(cors());
 // Use gzip/deflate compression middleware
 app.use(compression());
 
-// Define a simple health check route. If the server is running
-// we'll respond with a 200 OK.  If not, the server isn't healthy.
-app.get('/', (req, res) => {
-  // Clients shouldn't cache this response (always request it fresh)
-  // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
-  res.setHeader('Cache-Control', 'no-cache');
-
-  // Send a 200 'OK' response with info about our repo
-  res.status(200).json({
-    status: 'ok',
-    author,
-    // TODO: change this to use your GitHub username!
-    githubUrl: 'https://github.com/zlinzz/fragments',
-    version,
-  });
-});
+// Define our routes
+app.use('/', require('./routes'));
 
 // Add 404 middleware to handle any requests for resources that can't be found
 app.use((req, res) => {
