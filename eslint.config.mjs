7fa530d756b604e19d8504754fe353d7559b94d0
zlinzz <z332119551@gmail.com>
2024-09-19 21:52:13 -0400

    Configure ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,17 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+// eslint.config.mjs
 
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
