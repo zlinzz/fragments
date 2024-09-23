e8ba85c750fe4a413668135f650cc70400a9de15
zlinzz <z332119551@gmail.com>
2024-09-23 02:25:42 -0400

    Refactor to use the response.js functions

--- src/app.js
+++ src/app.js
@@ -17,6 +17,9 @@ const pino = require('pino-http')({
   logger,
 });
 
+// Import response functions
+const { createErrorResponse } = require('./response');
+
 // Create an express app instance we can use to attach middleware and HTTP routes
 const app = express();
 
@@ -41,13 +44,7 @@ app.use('/', require('./routes'));
 
 // Add 404 middleware to handle any requests for resources that can't be found
 app.use((req, res) => {
-  res.status(404).json({
-    status: 'error',
-    error: {
-      message: 'not found',
-      code: 404,
-    },
-  });
+  res.status(404).json(createErrorResponse(404, "not found"));
 });
 
 // Add error-handling middleware to deal with anything else
@@ -63,13 +60,7 @@ app.use((err, req, res, next) => {
     logger.error({ err }, `Error processing request`);
   }
 
-  res.status(status).json({
-    status: 'error',
-    error: {
-      message,
-      code: status,
-    },
-  });
+  res.status(status).json(createErrorResponse(status, message));
 });
 
 // Export our `app` so we can access it in server.js
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,9 @@ const { version, author } = require('../../package.json');
 // Our authentication middleware
 const { authenticate } = require('../auth');
 
+// Import response functions
+const { createSuccessResponse } = require('../response');
+
 // Create a router that we can use to mount our API
 const router = express.Router();
 
@@ -26,13 +29,11 @@ router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
   // Send a 200 'OK' response
-  res.status(200).json({
-    status: 'ok',
-    author,
-    // Use your own GitHub URL for this!
-    githubUrl: 'https://github.com/zlinzz/fragments',
-    version,
-  });
+  res.status(200).json(createSuccessResponse({
+    author, 
+    githubUrl: 'https://github.com/zlinzz/fragments', 
+    version
+  }));
 });
 
 module.exports = router;
