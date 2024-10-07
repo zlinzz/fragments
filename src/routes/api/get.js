4e5e7ef1842db179f81cca935539d4e40927864f
zlinzz <z332119551@gmail.com>
2024-10-07 00:46:20 -0400

    Add GET /fragments route

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,10 +1,17 @@
 // src/routes/api/get.js
 
-const { createSuccessResponse } = require("../../response");
-
+const { createSuccessResponse, createErrorResponse } = require('../../response');
+const { Fragment } = require('../../model/fragment');
+const logger = require('../../logger');
 /**
  * Get a list of fragments for the current user
  */
-module.exports = (req, res) => {
-  res.status(200).json(createSuccessResponse({fragments: []}));
-  };
+module.exports = async (req, res) => {
+  try {
+    const fragments = await Fragment.byUser(req.user);
+    res.status(200).json(createSuccessResponse({ fragments }));
+  } catch (err) {
+    logger.error({ err, ownerId: req.user }, 'Failed to get fragments');
+    res.status(500).json(createErrorResponse(500, 'Unable to retrieve fragments'));
+  }
+};
