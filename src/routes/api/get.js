223bd818e94f5f75c23440b0138998cbc83e1146
zlinzz <z332119551@gmail.com>
2024-10-28 22:53:15 -0400

    Add GET /fragments?expand=1 route

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -7,8 +7,9 @@ const logger = require('../../logger');
  * Get a list of fragments for the current user
  */
 module.exports = async (req, res) => {
+  const expand = req.query.expand === '1'; // Check if expand query parameter is present and equals '1'
   try {
-    const fragments = await Fragment.byUser(req.user);
+    const fragments = await Fragment.byUser(req.user, expand);
     res.status(200).json(createSuccessResponse({ fragments }));
   } catch (err) {
     logger.error({ err, ownerId: req.user }, 'Failed to get fragments');
