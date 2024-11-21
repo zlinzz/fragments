acf26c9a700d7af1cea8e45e6711d1d9e764031d
zlinzz <z332119551@gmail.com>
2024-11-21 14:27:46 -0500

    Update the err msg

--- src/routes/api/delete.js
+++ src/routes/api/delete.js
@@ -15,7 +15,7 @@ module.exports = async (req, res) => {
     // if Fragment.byID throw inside deleteFragment, return 404; Else 500
     if (err.message === 'Fragment by id not found') {
       logger.error({ ownerId, id }, 'Failed to delete fragment. Fragment not found.');
-      return res.status(404).json(createErrorResponse(404, 'Fragment not found'));
+      return res.status(404).json(createErrorResponse(404, err.message));
     }
     logger.error({ err }, 'Failed to delete fragment.');
     return res.status(500).json((500, 'An error occurred while deleting the fragment'));
